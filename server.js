6477ff24bb85c5e8f668c2021378fb7292e277c1
Nathan Rajlich <nathan@tootallnate.net>
2017-12-13 17:26:06 -0800

    Implement `GET /lts`

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const INDEX = 'http://nodejs.org/dist/index.json'
 
 module.exports = async (req, res) => {
   const { pathname, query } = parse(req.url, true)
-  const tag = query.tag || decodeURIComponent(pathname.substr(1)) || '*'
+  const tag = (
+    query.tag ||
+    decodeURIComponent(pathname.substr(1)) ||
+    '*'
+  ).toLowerCase()
   const match = await resolveVersion(tag)
 
   if (!match) {
@@ -26,16 +30,21 @@ module.exports = async (req, res) => {
 }
 
 async function resolveVersion(tag) {
+  const lts = tag === 'lts'
   const res = await fetch(INDEX)
 
   if (!res.ok) {
     // TODO: handle error response
   }
 
-  const body = await res.json()
+  let body = await res.json()
+  if (lts) {
+    body = body.filter(b => b.lts)
+  }
   const data = new Map(body.map(b => [b.version, b]))
   const versions = body.map(b => b.version)
-  const version = maxSatisfying(versions, tag)
+  const matchTag = lts ? '*' : tag
+  const version = maxSatisfying(versions, matchTag)
   if (!version) {
     return null
   }
