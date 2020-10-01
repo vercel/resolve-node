782b0ba88bc405d8cd9be7038dd10a56d4d00b6b
Nathan Rajlich <nathan@tootallnate.net>
2020-10-01 13:47:08 -0700

    Prettier

--- index.js
+++ index.js
@@ -5,74 +5,79 @@ const fetch = require('node-fetch')
 const { compare, maxSatisfying } = require('semver')
 
 const INDEX = 'https://nodejs.org/dist/index.json'
-const UNOFFICIAL = 'https://unofficial-builds.nodejs.org/download/release/index.json';
+const UNOFFICIAL =
+  'https://unofficial-builds.nodejs.org/download/release/index.json'
 
 async function getJson(url, unofficial) {
-  const res = await fetch(url);
+  const res = await fetch(url)
   if (!res.ok) {
-    throw new Error(`Received ${res.status} from ${url}: ${await res.text()}`);
+    throw new Error(`Received ${res.status} from ${url}: ${await res.text()}`)
   }
-  const body = await res.json();
+  const body = await res.json()
   for (const b of body) {
-    b.unofficial = unofficial;
+    b.unofficial = unofficial
   }
-  return body;
+  return body
 }
 
 async function resolveVersion(tag, opts) {
-  const [ dist, unofficial ] = await Promise.all([
+  const [dist, unofficial] = await Promise.all([
     getJson(INDEX, false),
-    getJson(UNOFFICIAL, true)
-  ]);
+    getJson(UNOFFICIAL, true),
+  ])
 
-  let body = [...dist, ...unofficial];
+  let body = [...dist, ...unofficial]
   body.sort((a, b) => {
-    const c = compare(a.version, b.version);
+    const c = compare(a.version, b.version)
     if (c !== 0) {
-      return c * -1;
+      return c * -1
     }
     if (a.unofficial && !b.unofficial) {
-      return -1;
+      return -1
     }
     if (!a.unofficial && b.unofficial) {
-      return 1;
+      return 1
     }
-    return 0;
-  });
+    return 0
+  })
 
   const ltsParts = tag.match(/^lts(?:\/([a-z]+))?$/)
   const isLts = ltsParts !== null
   if (isLts) {
     const codename = ltsParts[1]
-    body = body.filter(b => b.lts && (codename ? b.lts.toLowerCase() === codename : true))
+    body = body.filter(
+      (b) => b.lts && (codename ? b.lts.toLowerCase() === codename : true)
+    )
   }
 
   if (opts.security) {
-    body = body.filter(b => b.security)
+    body = body.filter((b) => b.security)
   }
 
   if (opts.platform && opts.arch) {
-    body = body.filter(b => b.files.includes(`${opts.platform}-${opts.arch}`));
+    body = body.filter((b) => b.files.includes(`${opts.platform}-${opts.arch}`))
   }
   //console.log(body);
 
-  const data = new Map(body.map(b => [b.version, b]))
-  const versions = new Set(body.map(b => b.version))
+  const data = new Map(body.map((b) => [b.version, b]))
+  const versions = new Set(body.map((b) => b.version))
   const matchTag = isLts ? '*' : tag
   const version = maxSatisfying(Array.from(versions), matchTag)
   if (!version) {
     return null
   }
   const match = data.get(version)
-  let url = undefined;
+  let url = undefined
   if (opts.platform && opts.arch) {
-    const urlBase = match.unofficial ? 'https://unofficial-builds.nodejs.org/download/release' : 'https://nodejs.org/dist';
-    url =  `${urlBase}/${match.version}/node-${match.version}-${opts.platform}-${opts.arch}.tar.gz`;
+    const urlBase = match.unofficial
+      ? 'https://unofficial-builds.nodejs.org/download/release'
+      : 'https://nodejs.org/dist'
+    url = `${urlBase}/${match.version}/node-${match.version}-${opts.platform}-${opts.arch}.tar.gz`
   }
   return Object.assign({ tag, url }, match)
 }
 
-async function handler (req, res) {
+async function handler(req, res) {
   const { pathname, query } = parse(req.url, true)
   const tag = (
     query.tag ||
@@ -80,18 +85,18 @@ async function handler (req, res) {
     '*'
   ).toLowerCase()
 
-  query.security = yn(query.security);
-  console.log({ query });
+  query.security = yn(query.security)
+  console.log({ query })
 
-  const match = await resolveVersion(tag, query);
-  console.log({ match });
+  const match = await resolveVersion(tag, query)
+  console.log({ match })
 
   if (!match) {
     res.statusCode = 404
     return {
       tag,
       query,
-      error: 'No match found'
+      error: 'No match found',
     }
   }
 
--- test/test.js
+++ test/test.js
@@ -8,109 +8,115 @@ const proxyquire = require('proxyquire')
 const micro = require('micro')
 const { test } = require('ava')
 
-const readJson = file => JSON.parse(fs.readFileSync(file, 'utf8'))
+const readJson = (file) => JSON.parse(fs.readFileSync(file, 'utf8'))
 
 const mockedFetch = fetchMock
   .sandbox()
-  .mock('https://nodejs.org/dist/index.json', readJson(`${__dirname}/fixtures/index.json`))
-  .mock('https://unofficial-builds.nodejs.org/download/release/index.json', readJson(`${__dirname}/fixtures/index.json`))
+  .mock(
+    'https://nodejs.org/dist/index.json',
+    readJson(`${__dirname}/fixtures/index.json`)
+  )
+  .mock(
+    'https://unofficial-builds.nodejs.org/download/release/index.json',
+    readJson(`${__dirname}/fixtures/index.json`)
+  )
 
 const api = proxyquire('..', {
-  'node-fetch': mockedFetch
+  'node-fetch': mockedFetch,
 })
 
-test.beforeEach(async t => {
+test.beforeEach(async (t) => {
   const service = (t.context.service = micro(api))
   t.context.url = await listen(service)
 })
 
-test.afterEach.always(async t => {
+test.afterEach.always(async (t) => {
   await t.context.service.close()
 })
 
-test('should prefer `text/plain`', async t => {
+test('should prefer `text/plain`', async (t) => {
   const { headers } = await fetch(t.context.url)
   t.is(headers.get('content-type'), 'text/plain')
 })
 
-test('should honor `application/json`', async t => {
+test('should honor `application/json`', async (t) => {
   const { headers } = await fetch(t.context.url, {
-    headers: { accept: 'application/json' }
+    headers: { accept: 'application/json' },
   })
   t.regex(headers.get('content-type'), /application\/json/)
 })
 
-test('should prefer query param over path segment', async t => {
+test('should prefer query param over path segment', async (t) => {
   const res = await fetch(`${t.context.url}/lts/Dubnium?tag=6.x`)
   t.is(await res.text(), 'v6.17.1')
 })
 
-test('should 404 on empty codename (query param)', async t => {
+test('should 404 on empty codename (query param)', async (t) => {
   const { status } = await fetch(`${t.context.url}/?tag=lts/`)
   t.is(status, 404)
 })
 
-test('should 404 on empty codename (path segment)', async t => {
+test('should 404 on empty codename (path segment)', async (t) => {
   const { status } = await fetch(`${t.context.url}/lts/`)
   t.is(status, 404)
 })
 
-test('/', async t => {
+test('/', async (t) => {
   const res = await fetch(t.context.url)
   t.is(await res.text(), 'v14.13.0')
 })
 
-test('/lts', async t => {
+test('/lts', async (t) => {
   const res = await fetch(`${t.context.url}/lts`)
   t.is(await res.text(), 'v12.18.4')
 })
 
-test('/lts/Carbon', async t => {
+test('/lts/Carbon', async (t) => {
   const res = await fetch(`${t.context.url}/lts/Carbon`)
   t.is(await res.text(), 'v8.17.0')
 })
 
-test('/8.x', async t => {
+test('/8.x', async (t) => {
   const res = await fetch(`${t.context.url}/8.x`)
   t.is(await res.text(), 'v8.17.0')
 })
 
-test('/?tag=lts', async t => {
+test('/?tag=lts', async (t) => {
   const res = await fetch(`${t.context.url}/?tag=lts`)
   t.is(await res.text(), 'v12.18.4')
 })
 
-test('/?tag=lts/Dubnium', async t => {
+test('/?tag=lts/Dubnium', async (t) => {
   const res = await fetch(`${t.context.url}/?tag=lts/Dubnium`)
   t.is(await res.text(), 'v10.22.1')
 })
 
-test('/?tag=8.11.x', async t => {
+test('/?tag=8.11.x', async (t) => {
   const res = await fetch(`${t.context.url}/?tag=8.11.x`)
   t.is(await res.text(), 'v8.11.4')
 })
 
-test('/?security=true', async t => {
+test('/?security=true', async (t) => {
   const res = await fetch(`${t.context.url}/?security=true`)
   t.is(await res.text(), 'v14.11.0')
 })
 
-test('/lts?security=true', async t => {
+test('/lts?security=true', async (t) => {
   const res = await fetch(`${t.context.url}/lts?security=true`)
   t.is(await res.text(), 'v12.18.4')
 })
 
-test('/lts/Carbon?security=true', async t => {
+test('/lts/Carbon?security=true', async (t) => {
   const res = await fetch(`${t.context.url}/lts/Carbon?security=true`)
   t.is(await res.text(), 'v8.17.0')
 })
 
-test('/6.x?security=true', async t => {
+test('/6.x?security=true', async (t) => {
   const res = await fetch(`${t.context.url}/6.x?security=true`)
   t.is(await res.text(), 'v6.17.0')
 })
 
-test('/13.x?security=true', async t => {
+test('/13.x?security=true', async (t) => {
   const res = await fetch(`${t.context.url}/13.x?security=true`)
   t.is(await res.text(), 'v13.8.0')
   t.is(res.status, 200)
