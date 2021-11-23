4f45d71730c4c09ba8cbbdadf4849059e386050a
Nathan Rajlich <n@n8.io>
2021-11-22 20:18:09 -0800

    Use x64 for Apple M1 for older Node.js versions (#18)

--- index.js
+++ index.js
@@ -1,148 +1,160 @@
-const yn = require('yn')
-const { run } = require('micro')
-const { parse } = require('url')
-const fetch = require('node-fetch')
-const { compare, maxSatisfying } = require('semver')
+const yn = require('yn');
+const { run } = require('micro');
+const { parse } = require('url');
+const fetch = require('node-fetch');
+const semver = require('semver');
 
-const INDEX = 'https://nodejs.org/dist/index.json'
+const INDEX = 'https://nodejs.org/dist/index.json';
 const UNOFFICIAL =
-  'https://unofficial-builds.nodejs.org/download/release/index.json'
+  'https://unofficial-builds.nodejs.org/download/release/index.json';
 
 async function getJson(url, unofficial) {
-  const res = await fetch(url)
+  const res = await fetch(url);
   if (!res.ok) {
-    throw new Error(`Received ${res.status} from ${url}: ${await res.text()}`)
+    throw new Error(`Received ${res.status} from ${url}: ${await res.text()}`);
   }
-  const body = await res.json()
+  const body = await res.json();
   for (const b of body) {
-    b.unofficial = unofficial
+    b.unofficial = unofficial;
   }
-  return body
+  return body;
 }
 
 async function resolveVersion(tag, opts) {
   const [dist, unofficial] = await Promise.all([
     getJson(INDEX, false),
     getJson(UNOFFICIAL, true),
-  ])
+  ]);
 
-  let body = [...dist, ...unofficial]
+  let body = [...dist, ...unofficial];
   body.sort((a, b) => {
-    const c = compare(a.version, b.version)
+    const c = semver.compare(a.version, b.version);
     if (c !== 0) {
-      return c * -1
+      return c * -1;
     }
     if (a.unofficial && !b.unofficial) {
-      return -1
+      return -1;
     }
     if (!a.unofficial && b.unofficial) {
-      return 1
+      return 1;
     }
-    return 0
-  })
+    return 0;
+  });
 
-  const ltsParts = tag.match(/^lts(?:\/([a-z]+))?$/)
-  const isLts = ltsParts !== null
+  const ltsParts = tag.match(/^lts(?:\/([a-z]+))?$/);
+  const isLts = ltsParts !== null;
   if (isLts) {
-    const codename = ltsParts[1]
+    const codename = ltsParts[1];
     body = body.filter(
       (b) => b.lts && (codename ? b.lts.toLowerCase() === codename : true)
-    )
+    );
   }
 
   if (opts.security) {
-    body = body.filter((b) => b.security)
+    body = body.filter((b) => b.security);
   }
 
-  const platform = getPlatform(opts.platform)
-  const arch = getArch(opts.arch)
+  const matchTag = isLts ? '*' : tag;
+
+  const platform = getPlatform(opts.platform);
+  let arch = getArch(opts.arch);
   if (platform && arch) {
-    let suffix = ''
+    let suffix = '';
     if (platform === 'osx') {
-      suffix = '-tar'
-    } else if (platform === 'win') {
-      suffix = '-zip'
-    }
-    body = body.filter((b) => b.files.includes(`${platform}-${arch}${suffix}`))
-  }
-  //console.log(body);
+      suffix = '-tar';
 
-  const data = new Map(body.map((b) => [b.version, b]))
-  const versions = new Set(body.map((b) => b.version))
-  const matchTag = isLts ? '*' : tag
-  const version = maxSatisfying(Array.from(versions), matchTag)
-  if (!version) {
-    return null
+      // "darwin/arm64" is Apple M1, which supports x64 binaries via
+      // Rosetta emulation. Since there's no arm64 binaries for
+      // older Node.js versions, return the x64 download URL.
+      try {
+        if (arch === 'arm64' && semver.lt(semver.coerce(matchTag), '16.0.0')) {
+          arch = 'x64';
+        }
+      } catch (_) {
+        console.log(_);
+        // ignore
+      }
+    } else if (platform === 'win') {
+      suffix = '-zip';
+    }
+    body = body.filter((b) => b.files.includes(`${platform}-${arch}${suffix}`));
   }
-  const match = data.get(version)
-  let url = undefined
-  if (opts.platform && opts.arch) {
+
+  const data = new Map(body.map((b) => [b.version, b]));
+  const versions = new Set(body.map((b) => b.version));
+  const version = semver.maxSatisfying(Array.from(versions), matchTag);
+  if (!version) {
+    return null;
+  }
+  const match = data.get(version);
+  let url = undefined;
+  if (opts.platform && arch) {
     const urlBase = match.unofficial
       ? 'https://unofficial-builds.nodejs.org/download/release'
-      : 'https://nodejs.org/dist'
-    url = `${urlBase}/${match.version}/node-${match.version}-${opts.platform}-${opts.arch}.tar.gz`
+      : 'https://nodejs.org/dist';
+    url = `${urlBase}/${match.version}/node-${match.version}-${opts.platform}-${arch}.tar.gz`;
   }
-  return Object.assign({ tag, url }, match)
+  return Object.assign({ tag, url }, match);
 }
 
-const PLATFORM_MAP = new Map([['darwin', 'osx']])
+const PLATFORM_MAP = new Map([['darwin', 'osx']]);
 
 function getPlatform(platform) {
-  const p = String(platform || '').toLowerCase()
-  return PLATFORM_MAP.get(p) || p
+  const p = String(platform || '').toLowerCase();
+  return PLATFORM_MAP.get(p) || p;
 }
 
-const ARCH_MAP = new Map([['x86_64', 'x64']])
+const ARCH_MAP = new Map([['x86_64', 'x64']]);
 
 function getArch(arch) {
-  const a = String(arch || '').toLowerCase()
-  return ARCH_MAP.get(a) || a
+  const a = String(arch || '').toLowerCase();
+  return ARCH_MAP.get(a) || a;
 }
 
 function parseAccept(req) {
-  return /json/.test(req.headers.accept) ? 'json' : 'text'
+  return /json/.test(req.headers.accept) ? 'json' : 'text';
 }
 
 async function handler(req, res) {
-  const { pathname, query } = parse(req.url, true)
-  const format = query.format || parseAccept(req)
+  const { pathname, query } = parse(req.url, true);
+  const format = query.format || parseAccept(req);
   let tag = (
     query.tag ||
     decodeURIComponent(pathname.substr(1)) ||
     '*'
-  ).toLowerCase()
+  ).toLowerCase();
 
   if (tag === 'latest') {
-    tag = '*'
+    tag = '*';
   }
 
-  query.security = yn(query.security)
+  query.security = yn(query.security);
 
-  const match = await resolveVersion(tag, query)
+  const match = await resolveVersion(tag, query);
 
   if (!match) {
-    res.statusCode = 404
+    res.statusCode = 404;
     return {
       tag,
       query,
       error: 'No match found',
-    }
+    };
   }
 
   if (match.url) {
-    res.setHeader('X-Download-URL', match.url)
+    res.setHeader('X-Download-URL', match.url);
   }
 
-  res.setHeader('X-Node-Version', match.version)
+  res.setHeader('X-Node-Version', match.version);
 
   if (format === 'json') {
-    return match
+    return match;
   } else if (format === 'text') {
-    res.setHeader('Content-Type', 'text/plain; charset=utf8')
-    return match.version
+    res.setHeader('Content-Type', 'text/plain; charset=utf8');
+    return match.version;
   } else {
-    throw new Error(`Unknown "format": ${format}`)
+    throw new Error(`Unknown "format": ${format}`);
   }
 }
 
-module.exports = (req, res) => run(req, res, handler)
+module.exports = (req, res) => run(req, res, handler);
